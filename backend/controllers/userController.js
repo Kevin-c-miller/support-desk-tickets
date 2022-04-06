bb05d2df48c347bedd5f4835a6403bd26ea0541a
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-06 00:28:17 -0400

    added an errorhandler function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,20 @@
+// Register a new user
+// @route /api/users
+// @access public
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+
+  //  validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
   res.send('register route');
 };
 
+// Login a user
+// @route /api/users/login
+// @access public
 const loginUser = (req, res) => {
   res.send('login route');
 };
