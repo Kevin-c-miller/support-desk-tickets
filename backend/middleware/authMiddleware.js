b5ffe82f33ca7dfe3aefe37288b7c17dbbc6dbd3
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-06 12:30:19 -0400

    commiting unsaved file, authmiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -13,7 +13,7 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      //   get token from header (splits into array, splitting at the space so it will look like ['bearer', 'token'] , token will be at the 1 index)
+      //  get token from header (splits into array, splitting at the space so it will look like ['bearer', 'token'] , token will be at the 1 index)
       token = req.headers.authorization.split(' ')[1];
 
       // verify token
