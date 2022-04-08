af326c760969c29ef3d9e8fcc7f06ef1a71fef35
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-08 11:40:41 -0400

    created get and create ticket conroller functions

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -6,14 +6,47 @@ const Ticket = require('../models/ticketModel');
 // @route GET /api/tickets/
 // @access private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'get Tickets' });
+  // get user using the id in the jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // create new user tickets
 // @route POST /api/tickets/
 // @access private
 const createTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'create ticket' });
+  // destructure product/description from request body
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please add an product and descripton');
+  }
+
+  // get user using the id in the jwt
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = {
