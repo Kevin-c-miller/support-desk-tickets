4e447c1a80e356b222ea07e26849e3e3ca185353
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-04-13 10:27:36 -0400

    added notesslice and notes service to redux state - get notes

---
+++ frontend/src/features/notes/notesService.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API_URL = 'api/tickets/';
+
+// get ticket notes
+const getNotes = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const res = await axios.get(API_URL + ticketId + '/notes', config);
+
+  return res.data;
+};
+
+const notesService = {
+  getNotes,
+};
+
+export default notesService;
